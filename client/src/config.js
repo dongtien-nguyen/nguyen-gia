65239be53845a44232823156a00325b3f7e90598
Tien Nguyen Dong <75530979+dongtien-nguyen@users.noreply.github.com>
2025-07-02 01:23:16 +0700

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API_URL = import.meta.env.VITE_API_URL || "https://nguyen-gia.azurewebsites.net";
 export default API_URL;
